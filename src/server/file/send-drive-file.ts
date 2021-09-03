7ed5a685aa1d6085bcddb287235c954e06158559
taraxos <89680839+taraxos@users.noreply.github.com>
2021-09-03 06:11:21 -0500

    Update send-drive-file.ts
    Change thumbnail dimensions

--- src/server/file/send-drive-file.ts
+++ src/server/file/send-drive-file.ts
@@ -63,9 +63,9 @@ export default async function(ctx: Koa.Context) {
 				const convertFile = async () => {
 					if (isThumbnail) {
 						if (['image/jpeg', 'image/webp'].includes(mime)) {
-							return await convertToJpeg(path, 498, 280);
+							return await convertToJpeg(path, 720, 512);
 						} else if (['image/png'].includes(mime)) {
-							return await convertToPngOrJpeg(path, 498, 280);
+							return await convertToPngOrJpeg(path, 720, 512);
 						} else if (mime.startsWith('video/')) {
 							return await GenerateVideoThumbnail(path);
 						}
