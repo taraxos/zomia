13aa5b16a3b873b1ae8063e68643d1cc796ecc87
taraxos <89680839+taraxos@users.noreply.github.com>
2021-09-17 14:00:53 -0500

    Update gen-avatar.ts
    Fixed errors

--- src/misc/gen-avatar.ts
+++ src/misc/gen-avatar.ts
@@ -8,22 +8,22 @@ import * as fs from 'fs';
 import { WriteStream } from 'fs';
 
 const colors = [
-	"#e57373",
-	"#f06292",
-	"#ba68c8",
-	"#9575cd",
-	"#7986cb",
-	"#64b5f6",
-	"#4fc3f7",
-	"#4dd0e1",
-	"#4db6ac",
-	"#81c784",
-	"#8bc34a",
-	"#afb42b",
-	"#f57f17",
-	"#ff5722",
-	"#795548",
-	"#455a64"
+	'#e57373',
+	'#f06292',
+	'#ba68c8',
+	'#9575cd',
+	'#7986cb',
+	'#64b5f6',
+	'#4fc3f7',
+	'#4dd0e1',
+	'#4db6ac',
+	'#81c784',
+	'#8bc34a',
+	'#afb42b',
+	'#f57f17',
+	'#ff5722',
+	'#795548',
+	'#455a64'
 ];
 
 /**
@@ -31,26 +31,26 @@ const colors = [
  */
 export function genAvatar(seed: string, stream: WriteStream): Promise<void> {
 	const rand = gen.create(seed);
-	
+
 	// throw the dice for body parts
 	const parts = [
-		["body", rand(15)],
-		["fur", rand(10)],
-		["eyes", rand(15)],
-		["mouth", rand(10)]
-	]
+		['body', rand(15)],
+		['fur', rand(10)],
+		['eyes', rand(15)],
+		['mouth', rand(10)]
+	];
 	
-	var canvas = p.make(256, 256);
-	var ctx = canvas.getcontext('2d');
-	
+	const canvas = p.make(256, 256);
+	const ctx = canvas.getcontext('2d');
+
 	ctx.fillStyle = colors[rand(colors.length)];
-	
+
 	// add parts
 	for (let part of parts) {
-		p.decodePNGFromStream(fs.createReadStream("img/"+part[0]+"_"+String(part[1])+".png")).then((png) => {
+		p.decodePNGFromStream(fs.createReadStream('img/'+part[0]+'_'+String(part[1])+'.png')).then((png) => {
 			ctx.drawImage(png, 0, 0);
 		});
-	};
-	
+	}
+
 	return p.encodePNGToStream(canvas, stream);
 }
