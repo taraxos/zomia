33be048f80f3c3f989a0ce7b2f7e41dcae3f2935
taraxos <89680839+taraxos@users.noreply.github.com>
2021-09-17 13:29:50 -0500

    Update gen-avatar.ts
    Changed avatar generator to use PepperCarrot's abstract art generator based on monsterID

--- src/misc/gen-avatar.ts
+++ src/misc/gen-avatar.ts
@@ -4,87 +4,53 @@
 
 import * as p from 'pureimage';
 import * as gen from 'random-seed';
+import * as fs from 'fs';
 import { WriteStream } from 'fs';
 
-const size = 256; // px
-const n = 5; // resolution
-const margin = (size / n);
 const colors = [
-	'#e57373',
-	'#F06292',
-	'#BA68C8',
-	'#9575CD',
-	'#7986CB',
-	'#64B5F6',
-	'#4FC3F7',
-	'#4DD0E1',
-	'#4DB6AC',
-	'#81C784',
-	'#8BC34A',
-	'#AFB42B',
-	'#F57F17',
-	'#FF5722',
-	'#795548',
-	'#455A64',
+	"#e57373",
+	"#e06292",
+	"#ba68c8",
+	"#9575cd",
+	"#7986cb",
+	"#64b5f6",
+	"#4fc3f7",
+	"#4dd0e1",
+	"#4db6ac",
+	"#81c784",
+	"#8bc34a",
+	"#afb42b",
+	"#f57f17",
+	"#ff5722",
+	"#795548",
+	"#455a64"
 ];
-const bg = '#e9e9e9';
-
-const actualSize = size - (margin * 2);
-const cellSize = actualSize / n;
-const sideN = Math.floor(n / 2);
 
 /**
  * Generate buffer of random avatar by seed
  */
-export function genAvatar(seed: string, stream: WriteStream): Promise<void> {
+export function genAvatar(stream: WriteStream): Promise<void> {
 	const rand = gen.create(seed);
-	const canvas = p.make(size, size);
-	const ctx = canvas.getContext('2d');
-
-	ctx.fillStyle = bg;
-	ctx.beginPath();
-	ctx.fillRect(0, 0, size, size);
-
+	
+	// throw the dice for body parts
+	const parts = [
+		["body", rand(15)],
+		["fur", rand(10)],
+		["eyes", rand(15)],
+		["mouth", rand(10)]
+	]
+	
+	var canvas = p.make(256, 256);
+	var ctx = canvas.getcontext('2d');
+	
 	ctx.fillStyle = colors[rand(colors.length)];
-
-	// side bitmap (filled by false)
-	const side: boolean[][] = new Array(sideN);
-	for (let i = 0; i < side.length; i++) {
-		side[i] = new Array(n).fill(false);
-	}
-
-	// 1*n (filled by false)
-	const center: boolean[] = new Array(n).fill(false);
-
-	// tslint:disable-next-line:prefer-for-of
-	for (let x = 0; x < side.length; x++) {
-		for (let y = 0; y < side[x].length; y++) {
-			side[x][y] = rand(3) === 0;
-		}
-	}
-
-	for (let i = 0; i < center.length; i++) {
-		center[i] = rand(3) === 0;
-	}
-
-	// Draw
-	for (let x = 0; x < n; x++) {
-		for (let y = 0; y < n; y++) {
-			const isXCenter = x === ((n - 1) / 2);
-			if (isXCenter && !center[y]) continue;
-
-			const isLeftSide = x < ((n - 1) / 2);
-			if (isLeftSide && !side[x][y]) continue;
-
-			const isRightSide = x > ((n - 1) / 2);
-			if (isRightSide && !side[sideN - (x - sideN)][y]) continue;
-
-			const actualX = margin + (cellSize * x);
-			const actualY = margin + (cellSize * y);
-			ctx.beginPath();
-			ctx.fillRect(actualX, actualY, cellSize, cellSize);
-		}
-	}
-
+	
+	// add parts
+	for (let part of parts) {
+		p.decodePNGFromStream(fs.createReadStream("img/"+part[0]+"_"+String(part[1])+".png")).then((png) => {
+			ctx.drawImage(png, 0, 0);
+		});
+	};
+	
 	return p.encodePNGToStream(canvas, stream);
 }
