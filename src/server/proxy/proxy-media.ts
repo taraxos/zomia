e67a721341656efb11fdac9aa2759275fdc430ee
taraxos <89680839+taraxos@users.noreply.github.com>
2021-09-18 00:26:39 -0500

    Update proxy-media.ts
    Fixed thumbnails

--- src/server/proxy/proxy-media.ts
+++ src/server/proxy/proxy-media.ts
@@ -22,7 +22,7 @@ export async function proxyMedia(ctx: Koa.Context) {
 		let image: IImage;
 
 		if ('static' in ctx.query && ['image/png', 'image/gif', 'image/apng', 'image/vnd.mozilla.apng', 'image/webp'].includes(mime)) {
-			image = await convertToPng(path, 1024, 720);
+			image = await convertToPng(path, 640, 912);
 		} else if ('preview' in ctx.query && ['image/jpeg', 'image/png', 'image/gif', 'image/apng', 'image/vnd.mozilla.apng'].includes(mime)) {
 			image = await convertToJpeg(path, 200, 200);
 		} else {
