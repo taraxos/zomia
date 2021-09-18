c14c6c81bb0fcc85b694f09ade5a8d8792571fe8
taraxos <89680839+taraxos@users.noreply.github.com>
2021-09-18 16:11:25 -0500

    Revert "Use canvas instead of pureimage"
    This reverts commit 0659582179085254dad257ea1ba06f55a5661794.

--- src/misc/gen-avatar.ts
+++ src/misc/gen-avatar.ts
@@ -2,8 +2,10 @@
  * Random avatar generator
  */
 
-import { createCanvas, loadImage } from 'canvas';
+import * as p from 'pureimage';
 import * as gen from 'random-seed';
+import * as fs from 'fs';
+import { WriteStream } from 'fs';
 
 const size = 256; // px
 const colors = [
@@ -28,10 +30,8 @@ const colors = [
 /**
  * Generate buffer of random avatar by seed
  */
-export function genAvatar(seed: string) {
+export function genAvatar(seed: string, stream: WriteStream): Promise<void> {
 	const rand = gen.create(seed);
-	const canvas = createCanvas(size, size);
-	const ctx = canvas.getContext('2d');
 
 	// throw the dice for body parts
 	const parts = [
@@ -41,15 +41,28 @@ export function genAvatar(seed: string) {
 		['mouth', rand(10)]
 	];
 
+	const canvas = p.make(size, size);
+	const ctx = canvas.getContext('2d');
+
 	ctx.fillStyle = colors[rand(colors.length)];
 	ctx.beginPath();
 	ctx.fillRect(0, 0, size, size);
+	
+	p.decodePNGFromStream(fs.createReadStream('img/body_1.png')).then((img) => {
+		ctx.drawImage(img, 0, 0, 256, 256);
+	});
 
+	// add parts
+	/*
 	for (let part of parts) {
-		loadImage('img/'+part[0]+'_'+String(part[1])+'.png').then((img) => {
-			ctx.drawImage(img, 0, 0, size, size);
+		p.decodePNGFromStream(fs.createReadStream('/img/'+part[0]+'_'+String(part[1])+'.png')).then((img) => {
+			ctx.drawImage(img,
+				0, 0, 256, 256,
+				0, 0, size, size
+			);
 		});
 	}
+ */
 
-	return canvas.toBuffer();
+	return p.encodePNGToStream(canvas, stream);
 }
