53f4213542ee02d9ab4eddd6beaed9c8b84a2c64
taraxos <89680839+taraxos@users.noreply.github.com>
2021-09-22 09:40:52 -0500

    Update generate-replies-query.ts

--- src/server/api/common/generate-replies-query.ts
+++ src/server/api/common/generate-replies-query.ts
@@ -6,8 +6,9 @@ export function generateRepliesQuery(q: SelectQueryBuilder<any>, me?: { id: User
 		q.andWhere(new Brackets(qb => { qb
 			.where(`note.replyId IS NULL`) // 返信ではない
 			.orWhere(new Brackets(qb => { qb // 返信だけど投稿者自身への返信
-				.where(`note.replyId IS NOT NULL`)
-				.andWhere('note.replyUserId = note.userId');
+				.where(`note.replyId IS NOT NULL`);
+				// fix bug where some replies aren't shown in TL
+				// .andWhere('note.replyUserId = note.userId');
 			}));
 		}));
 	} else {
@@ -15,12 +16,12 @@ export function generateRepliesQuery(q: SelectQueryBuilder<any>, me?: { id: User
 			.where(`note.replyId IS NULL`) // 返信ではない
 			.orWhere('note.replyUserId = :meId', { meId: me.id }) // 返信だけど自分のノートへの返信
 			.orWhere(new Brackets(qb => { qb // 返信だけど自分の行った返信
-				.where(`note.replyId IS NOT NULL`)
+				.where(`note.replyId IS NOT NULL`);
 				.andWhere('note.userId = :meId', { meId: me.id });
 			}))
 			.orWhere(new Brackets(qb => { qb // 返信だけど投稿者自身への返信
-				.where(`note.replyId IS NOT NULL`)
-				.andWhere('note.replyUserId = note.userId');
+				.where(`note.replyId IS NOT NULL`);
+				// .andWhere('note.replyUserId = note.userId');
 			}));
 		}));
 	}
